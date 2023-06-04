54d41f07bb2f9bcfe1983b4b8afede052fb6846b
Valeri Serafimov <v.serafimov@v.serafimov-H5QY77D0M4>
2023-06-04 03:35:49 +0300

    Created function to check for last timestamp from influxdb

--- extractor.py
+++ extractor.py
@@ -1,5 +1,5 @@
 import ccxt, pandas
-import time
+import time, datetime
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 import values
@@ -14,7 +14,7 @@ crypto_symbol = values.CRYPTO_PAIR
 crypto_timeframe = values.CRYPTO_TIMEFRAME
 
 ## Uncoment the following line to extract ohlcv data for particular date
-# crypto_since = since = int(datetime.datetime.strptime(values.CRYPTO_SINCE, "%Y-%m-%d %H:%M:%S%z").timestamp() * 1000) 
+# crypto_since = int(datetime.datetime.strptime(values.CRYPTO_SINCE, "%Y-%m-%d %H:%M:%S%z").timestamp() * 1000) 
 
 
 ## Initialize Binance ccxt and load markets
@@ -22,6 +22,28 @@ exchange = ccxt.binance()
 exchange.enableRateLimit = True
 exchange_markets = exchange.load_markets()
 
+## Initialize influxdb client 
+influx_client = InfluxDBClient(
+    url=url,
+    token=token,
+    org=org,
+    # debug=True
+    )
+
+## Initialize influxdb query api
+query_api = influx_client.query_api()
+
+## Get the last timestamp from influx
+def latest_timestamp(pair, timeframe): 
+    query = f'from(bucket: "{bucket}")\
+        |> range(start: 0)\
+        |> filter(fn: (r) => r["_measurement"] == "{pair}" and r.timeframe == "{timeframe}" and r._field == "_volume")\
+        |> last()'
+    
+    result = query_api.query(org=org, query=query)
+    last_time = list(result)[0].records[0].values["_time"]
+    return int(datetime.datetime.strptime(str(last_time), "%Y-%m-%d %H:%M:%S%z").timestamp() * 1000) 
+    
 
 ## GET and reform dataframe for infuxdb. Set timestamp as index
 def ohlcv_to_df(crypto_pair, pair_timeframe):
@@ -29,8 +51,8 @@ def ohlcv_to_df(crypto_pair, pair_timeframe):
     ohlcv_raw = exchange.fetch_ohlcv(
         symbol=crypto_pair,
         timeframe=pair_timeframe,
-        # since=since
-        # limit=1000
+        since=latest_timestamp(pair=crypto_pair, timeframe=pair_timeframe),
+        limit=1000
         )
     ohlcv_dataframe = pandas.DataFrame(
         ohlcv_raw,
@@ -59,13 +81,6 @@ def ohlcv_to_df(crypto_pair, pair_timeframe):
 
     yield data_points
 
-## Initialize influxdb client 
-influx_client = InfluxDBClient(
-    url=url,
-    token=token,
-    org=org,
-    # debug=True
-    )
 
 write_client = influx_client.write_api(write_options=SYNCHRONOUS)
 
@@ -81,8 +96,8 @@ if len(crypto_timeframe) != 0 or 1 :
                     write_precision="ms"
                 )
                 print(pair + " - " + p_timeframe + "; Pulled")
-        print("Sleeping 30s")
-        time.sleep(30)
+        print("Sleeping 10s")
+        time.sleep(10)
     
     else:
         # To do.
